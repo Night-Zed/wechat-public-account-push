9c64a527def292a5859af5ded4f62f5e9c96832c
Night-Zed <1525596821@qq.com>
2024-04-08 17:00:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,6 +5,55 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wx650c2319a6049318","APP_SECRET":"ec08749701fe1fe350be27fffe156bfa","IS_SHOW_COLOR":false,"CALLBACK_TEMPLATE_ID":"yHvvJjj16iJvIDaZBkr9ZVLAg6XKus9y4XAD9pSsINA","CALLBACK_USERS":[{"name":"自己","id":"oezfb6HU50gWITllfrmzyvoaODXk"}],"USERS":[{"name":"小颖bb","id":"oezfb6HU50gWITllfrmzyvoaODXk","useTemplateId":"yHvvJjj16iJvIDaZBkr9ZVLAg6XKus9y4XAD9pSsINA","province":"河北","city":"崇礼区","horoscopeDate":"09-04","horoscopeDateType":"今日","openUrl":"https://baidu.com/","festivals":[{"type":"*生日","name":"杰哥的猪猪小宝生日","date":"09-04","year":"2000"}],"customizedDateList":[{"keyword":"恋爱纪念日","date":"2022-01-31"}]}],"SWITCH":{"weather":true,"holidaytts":false,"CIBA":false,"oneTalk":false,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+const USER_CONFIG = {
+    "APP_ID": "wx650c2319a6049318",
+    "APP_SECRET": "ec08749701fe1fe350be27fffe156bfa",
+    "IS_SHOW_COLOR": false,
+    "CALLBACK_TEMPLATE_ID": "yHvvJjj16iJvIDaZBkr9ZVLAg6XKus9y4XAD9pSsINA",
+    "CALLBACK_USERS": [
+        {
+            "name": "自己",
+            "id": "oezfb6HU50gWITllfrmzyvoaODXk"
+        }
+    ],
+    "USERS": [
+        {
+            "name": "小颖bb",
+            "id": "oezfb6HU50gWITllfrmzyvoaODXk",
+            "useTemplateId": "yHvvJjj16iJvIDaZBkr9ZVLAg6XKus9y4XAD9pSsINA",
+            "province": "河北",
+            "city": "崇礼区",
+            "horoscopeDate": "09-04",
+            "horoscopeDateType": "今日",
+            "openUrl": "https://baidu.com/",
+            "festivals": [
+                {
+                    "type": "*生日",
+                    "name": "杰哥的猪猪小宝生日",
+                    "date": "09-04",
+                    "year": "2000"
+                }
+            ],
+            "customizedDateList": [
+                {
+                    "keyword": "恋爱纪念日",
+                    "date": "2022-01-31"
+                }
+            ]
+        }
+    ],
+    "SWITCH": {
+        "weather": true,
+        "holidaytts": false,
+        "CIBA": false,
+        "oneTalk": false,
+        "earthyLoveWords": true,
+        "momentCopyrighting": false,
+        "poisonChickenSoup": false,
+        "poetry": false,
+        "horoscope": true,
+        "birthdayMessage": true
+    }
+}
 module.exports = USER_CONFIG
 
